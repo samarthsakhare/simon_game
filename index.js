fe2eb6ed6cf543a33d77844d3c8c0f7d4a63c602
samarthsakhare <samarthsakhare7194@gmail.com>
2022-11-03 22:54:36 +0530

    added a all file of simon game

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
+
+nextSequence();
